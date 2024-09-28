30559076dce425a5497f36f2fb7389e632050bf1
Søren Ragsdale <sorenr@users.noreply.github.com>
2024-09-28 00:03:12 -0700

    added argparse

---
+++ wzraffle.py
@@ -0,0 +1,27 @@
+import argparse
+import random
+import time
+
+candidates = [
+    'Paul',
+    'Elon',
+    'Cris',
+    'XRay',
+    'Greg'
+]
+
+def raffle( seed = None ):
+    if not seed:
+        seed = int(1000*time.time())
+
+    random.seed(seed)
+
+    winner = random.choice(candidates)
+    print(f"Seed {seed} = {winner}")
+
+if __name__ == "__main__":
+    parser = argparse.ArgumentParser(description='Pick a winner.')
+    parser.add_argument('-s', dest='seed', type=int, help="seed value")
+    args = parser.parse_args()
+
+    raffle( args.seed )
